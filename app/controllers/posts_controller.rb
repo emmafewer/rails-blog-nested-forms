d90a24e1e0a6aa5dbc7ff14941a46e99014a3364
emmafewer <emmafewer20@gmail.com>
2020-12-22 14:08:23 -0800

    done

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -24,9 +24,13 @@ class PostsController < ApplicationController
   # POST /posts
   # POST /posts.json
   def create
-    @post = Post.new(post_params)
+    @post = Post.create(post_params)
+    if params["post"]["tags_attributes"] != nil 
+      new_tag = Tag.create(name: params["post"]["tags_attributes"]["name"])
+      PostTag.create(post_id: @post.id, tag_id: new_tag.id)
+    end
     respond_to do |format|
-      if @post.save
+      if @post.valid?
         format.html { redirect_to @post, notice: 'Post was successfully created.' }
         format.json { render action: 'show', status: :created, location: @post }
       else
@@ -68,6 +72,6 @@ class PostsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def post_params
-      params.require(:post).permit(:name, :content, :tag_ids => [])
+      params.require(:post).permit(:name, :content, :tag_ids => [], :tags_attributes => [])
     end
 end
